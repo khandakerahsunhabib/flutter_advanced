971c8bec8aef06383de8d7588774b4d563593673
khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2025-05-06 20:34:03 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
